ae9e6924104381c1cb71b8311006f0fc0bc8a0df
Luan Nico <luanpotter28@hotmail.com>
2015-04-26 23:25:55 -0300

    fetching from Bovespa - sort of

--- src/main/java/xyz/luan/apps/stocker/controller/StocksController.java
+++ src/main/java/xyz/luan/apps/stocker/controller/StocksController.java
@@ -3,6 +3,7 @@ package xyz.luan.apps.stocker.controller;
 import java.util.List;
 
 import xyz.luan.apps.stocker.main.StockContext;
+import xyz.luan.apps.stocker.util.BovespaRequest;
 import xyz.luan.console.fn.FnController;
 import xyz.luan.console.parser.Controller;
 import xyz.luan.console.parser.actions.Action;
@@ -20,8 +21,16 @@ public class StocksController extends Controller<StockContext> {
 		return CallResult.SUCCESS;
 	}
 
+	@Action("fetch")
+	public CallResult fetch(String stock) {
+		final BovespaRequest req = new BovespaRequest();
+		console.result(String.format("Stock value for %s is: %s", stock, req.getCurrentValue(stock)));
+		return CallResult.SUCCESS;
+	}
+
 	public static void defaultCallables(String name, List<Callable> callables) {
 		callables.add(new ActionCall(name + ":list", ":stocks", "List all stocks"));
+		callables.add(new ActionCall(name + ":fetch", ":fetch stock", "Fetch current value for a specific stock"));
 	}
 
 }
---
+++ src/main/java/xyz/luan/apps/stocker/util/BovespaRequest.java
@@ -0,0 +1,55 @@
+package xyz.luan.apps.stocker.util;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.filter.Filters;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.xpath.XPathExpression;
+import org.jdom2.xpath.XPathFactory;
+
+public class BovespaRequest {
+
+	private static final String URL = "http://www.bmfbovespa.com.br/Pregao-Online/ExecutaAcaoAjax.asp";
+	private static final String CODE = "CodigoPapel";
+
+	public double getCurrentValue(String stock) {
+		try {
+			HttpClient httpclient = HttpClients.createDefault();
+			HttpGet req = new HttpGet(new URIBuilder(URL).setParameter(CODE, stock).build());
+
+			HttpResponse response = httpclient.execute(req);
+			HttpEntity entity = response.getEntity();
+
+			if (entity != null) {
+				return extractValueFromResponseEntity(entity);
+			} else {
+				throw new IOException("Response expected, none returned...");
+			}
+		} catch (IOException | IllegalStateException | JDOMException | URISyntaxException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	private double extractValueFromResponseEntity(HttpEntity entity) throws JDOMException, IOException {
+		final SAXBuilder jdomBuilder = new SAXBuilder();
+		final XPathFactory xFactory = XPathFactory.instance();
+
+		Document doc = jdomBuilder.build(entity.getContent());
+		XPathExpression<Element> expr = xFactory.compile("//ComportamentoPapeis/Papel", Filters.element());
+		return convertToDouble(expr.evaluate(doc).get(0).getAttribute("Medio").getValue());
+	}
+
+	private double convertToDouble(String value) {
+		return Double.parseDouble(value.replace(',', '.'));
+	}
+}
